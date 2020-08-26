261503ce81e9f083234bc84d749d566f841ca414
Erik Ellis <elliserik69@gmail.com>
2020-08-25 21:46:52 -0400

    In the process of getting to read and write to a file

--- DNDCalendarTracker/src/CreateCalendar.java
+++ DNDCalendarTracker/src/CreateCalendar.java
@@ -1,21 +1,39 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class CreateCalendar {
 
+    private FileWriter myFile = new FileWriter("currentdate");
     private String[] months = {"Harnen", "Chay", "Anderik", "Miretorn", "Elarin", "Trakerule",
             "Mareasis", "Ulntar", "Krythrin", "Niilhin", "Eas", "Faldient"};
 
     private final int endOfMonth = 30;
     private int currentDay = 0;
     private int currentMonth = 0;
-    private int currentYear = 749;
+    private int currentYear = 0;
 
-    public CreateCalendar() {
-        currentDay = 1;
-        currentMonth = 0;
-    }
-
-    public CreateCalendar(int setDay, int setMonth){
+    public CreateCalendar(int setDay, int setMonth) throws IOException {
         currentDay =  setDay;
         currentMonth = setMonth;
+    }
+
+    public CreateCalendar() throws IOException {
+        File file = new File("C:\\Users\\erike\\IdeaProjects\\DNDCalendarTracker\\DNDCalendarTracker\\currentdate.txt");
+        try {
+            String line0 = Files.readAllLines(Paths.get("currentdate.txt")).get(0);
+            currentMonth = Integer.parseInt(line0);
+            String line1 = Files.readAllLines(Paths.get("currentdate.txt")).get(1);
+            currentDay = Integer.parseInt(line1);
+            String line2 = Files.readAllLines(Paths.get("currentdate.txt")).get(2);
+            currentYear = Integer.parseInt(line2);
+
+        } catch (IOException e){
+            System.out.println(e);
+        }
     }
 
     public String[] getMonths(){
--- DNDCalendarTracker/src/Main.java
+++ DNDCalendarTracker/src/Main.java
@@ -1,38 +1,44 @@
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String [] args){
-        CreateCalendar calendar = new CreateCalendar();
-        Scanner kb = new Scanner(System.in);
-        int originalDay = calendar.getCurrentDay();
-        int daysPassed;
+    public static void main(String[] args) {
+        try {
+            CreateCalendar calendar = new CreateCalendar();
+            Scanner kb = new Scanner(System.in);
+            int originalDay = calendar.getCurrentDay();
+            int daysPassed;
 
-        System.out.println("Current date: " + calendar.getMonthName(calendar.getCurrentMonth()) + "/"
-                                            + calendar.getCurrentDay() + "/" + calendar.getCurrentYear());
-        System.out.println("How many days passed this session?: ");
-        daysPassed = kb.nextInt();
-        calendar.setCurrentDay(calendar.getCurrentDay() + daysPassed);
+            System.out.println("Current date: " + calendar.getMonthName(calendar.getCurrentMonth()) + "/"
+                + calendar.getCurrentDay() + "/" + calendar.getCurrentYear());
+            System.out.println("How many days passed this session?: ");
+            daysPassed = kb.nextInt();
+            calendar.setCurrentDay(calendar.getCurrentDay() + daysPassed);
 
-        //test to see if month ended
-        if (calendar.getCurrentDay() > calendar.getEndOfMonth()){
-            int newDaysPassed = daysPassed % calendar.getEndOfMonth();
-            calendar.setCurrentDay(originalDay + newDaysPassed);
-            int monthsPassed = (daysPassed - newDaysPassed)/calendar.getEndOfMonth();
-            //test to see if year ended
-            if (monthsPassed == 12) {
-                calendar.setCurrentYear(calendar.getCurrentYear() + 1);
-                calendar.setCurrentMonth(calendar.getCurrentMonth());
-            } else if (monthsPassed > 12){
-                calendar.setCurrentYear(calendar.getCurrentYear() + (monthsPassed/12));
-                calendar.setCurrentMonth(calendar.getCurrentMonth() + (monthsPassed % 12));
-            } else {
-                calendar.setCurrentMonth(calendar.getCurrentMonth() + monthsPassed);
+            //test to see if month ended
+            if (calendar.getCurrentDay() > calendar.getEndOfMonth()) {
+                int newDaysPassed = daysPassed % calendar.getEndOfMonth();
+                calendar.setCurrentDay(originalDay + newDaysPassed);
+                int monthsPassed = (daysPassed - newDaysPassed) / calendar.getEndOfMonth();
+                //test to see if year ended
+                if (monthsPassed == 12) {
+                    calendar.setCurrentYear(calendar.getCurrentYear() + 1);
+                    calendar.setCurrentMonth(calendar.getCurrentMonth());
+                } else if (monthsPassed > 12) {
+                    calendar.setCurrentYear(calendar.getCurrentYear() + (monthsPassed / 12));
+                    calendar.setCurrentMonth(calendar.getCurrentMonth() + (monthsPassed % 12));
+                } else {
+                    calendar.setCurrentMonth(calendar.getCurrentMonth() + monthsPassed);
+                }
             }
-        }
 
-        System.out.println("New date: " + calendar.getMonthName(calendar.getCurrentMonth()) + "/"
-                                        + calendar.getCurrentDay() + "/" + calendar.getCurrentYear());
+            System.out.println("New date: " + calendar.getMonthName(calendar.getCurrentMonth()) + "/"
+                + calendar.getCurrentDay() + "/" + calendar.getCurrentYear());
+        }
+        catch (IOException e){
+            System.out.println(e);
+        }
     }
 }
